93bd14d6204b80cdceeb0d29b1478dd19be0b5d2
nidoos <hsoobin21@gmail.com>
2021-05-27 13:31:10 +0900

    미들웨어 만들기, 정적파일 서비스

--- main.js
+++ main.js
@@ -9,54 +9,60 @@ var path = require('path');
 var qs = require('querystring');
 var compression = require('compression');
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false })); //bodyparser가 실행되면서 결과로 미들웨어가 들어오게된다. bodyparser가 만들어내는 미들웨어를 표현하는 표현식.
 app.use(compression());
+app.get('*', (request, response, next) => {
+  fs.readdir('./data', (error, filelist) => {
+    request.list = filelist;
+    next();
+  });
+});// get방식으로 들어오는 요청에 대해서만 파일목록을 가져옴
 
 /*app.get : route, routing 하고있음
   if(pathname==='/'){} 와 같은 말
 */
 app.get('/', (request, response) => {
-  fs.readdir('./data', (error, filelist) => {
-    var title = 'Welcome';
-    var description = 'Hello, Node.js';
-    var list = template.list(filelist);
-    var html = template.HTML(title, list,
-      `<h2>${title}</h2>${description}`,
-      `<a href="/create">create</a>`
-    );
-    response.send(html);
-  });
+  // fs.readdir('./data', (error, filelist) => {
+  var title = 'Welcome';
+  var description = 'Hello, Node.js';
+  var list = template.list(request.list);
+  var html = template.HTML(title, list,
+    `<h2>${title}</h2>${description}
+    <img src="/images/hi.jpg" style ="width:300px; display:block; margin-top:10px">`,
+    `<a href="/create">create</a>`
+  );
+  response.send(html);
 });
 
-app.get('/page/:pageId', function (request, response) {
-  fs.readdir('./data', function (error, filelist) {
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-      var title = request.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ['h1']
-      });
-      var list = template.list(filelist);
-      var html = template.HTML(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
+app.get('/page/:pageId', (request, response) => {
+  //  fs.readdir('./data', (error, filelist) => {
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
+    var title = request.params.pageId;
+    var sanitizedTitle = sanitizeHtml(title);
+    var sanitizedDescription = sanitizeHtml(description, {
+      allowedTags: ['h1']
+    });
+    var list = template.list(request.list);
+    var html = template.HTML(sanitizedTitle, list,
+      `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+      ` <a href="/create">create</a>
                 <a href="/update/${sanitizedTitle}">update</a>
                 <form action="/delete_process" method="post">
                   <input type="hidden" name="id" value="${sanitizedTitle}">
                   <input type="submit" value="delete">
                 </form>`
-      );
-      response.send(html);
-    });
+    );
+    response.send(html);
   });
 });
 
 app.get('/create', (request, response) => {
-  fs.readdir('./data', (error, filelist) => {
-    var title = 'WEB - create';
-    var list = template.list(filelist);
-    var html = template.HTML(title, list, `
+  //fs.readdir('./data', (error, filelist) => {
+  var title = 'WEB - create';
+  var list = template.list(request.list);
+  var html = template.HTML(title, list, `
           <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
@@ -67,27 +73,11 @@ app.get('/create', (request, response) => {
             </p>
           </form>
         `, '');
-    response.send(html);
-  });
+  response.send(html);
+
 });
 
 app.post('/create_process', (request, response) => {
-  /*
-   var body = '';
-   request.on('data', (data) => {
-     body = body + data;
-   });
-   request.on('end', () => {
-     var post = qs.parse(body);
-     var title = post.title;
-     var description = post.description;
-     fs.writeFile(`data/${title}`, description, 'utf8', (err) => {
-       response.writeHead(302, { Location: `/?id=${title}` });
-       response.end();
-     })
-   });
-   */
-
   //bodyParser 사용
   var post = request.body;
   var title = post.title;
@@ -100,13 +90,13 @@ app.post('/create_process', (request, response) => {
 
 
 app.get('/update/:pageId', (request, response) => {
-  fs.readdir('./data', (error, filelist) => {
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
-      var title = request.params.pageId;
-      var list = template.list(filelist);
-      var html = template.HTML(title, list,
-        `
+  // fs.readdir('./data', (error, filelist) => {
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
+    var title = request.params.pageId;
+    var list = template.list(request.list);
+    var html = template.HTML(title, list,
+      `
             <form action="/update_process" method="post">
               <input type="hidden" name="id" value="${title}">
               <p><input type="text" name="title" placeholder="title" value="${title}"></p>
@@ -118,22 +108,13 @@ app.get('/update/:pageId', (request, response) => {
               </p>
             </form>
             `,
-        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-      );
-      response.send(html);
-    });
+      `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+    );
+    response.send(html);
   });
-
 });
 
 app.post('/update_process', (request, response) => {
-  /*
-    var body = '';
-    request.on('data', (data) => {
-      body = body + data;
-    });
-    request.on('end', () => {
-    */
   //bodyParser 사용
   var post = request.body;
   var id = post.id;
@@ -147,20 +128,12 @@ app.post('/update_process', (request, response) => {
 });
 
 app.post('/delete_process', (request, response) => {
-  /*
-    var body = '';
-    request.on('data', (data) => {
-      body = body + data;
-    });
-    request.on('end', () => {
-    */
   var post = request.body;
   var id = post.id;
   var filteredId = path.parse(id).base;
   fs.unlink(`data/${filteredId}`, (error) => {
     response.redirect('/');
   })
-
 });
 
 
